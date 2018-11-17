7b41cf4285d8f6c3f9618902e1659f53bab2766f
Matthew Clairmont <matthew.clairmont1@gmail.com>
2018-11-17 17:08:40 -0500

    Added Bambi Pistol and Ammo

--- ExileServer/addons/exile_server_config/config.cpp
+++ ExileServer/addons/exile_server_config/config.cpp
@@ -5662,7 +5662,10 @@ class CfgSettings
 			"ItemMap", // Because why not
 			"Exile_Item_XM8",
 			"ItemRadio",
-			"Exile_Item_PlasticBottleFreshWater"
+			"Exile_Item_Beer"
+			"Exile_Weapon_Makarov"
+			"Exile_Magazine_8Rnd_9x18"
+			"acc_flashlight"
 		};
 
 		/**
